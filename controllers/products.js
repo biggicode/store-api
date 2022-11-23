30db05c81e6c58db19b1911616e6aadbd7b487e8
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-23 23:28:44 +0200

    limit and page

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort("name")
-    .select("name price")
-    .limit(4)
-    .skip(1);
+  const products = await Product.find({}).sort("name").select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -37,6 +33,11 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
