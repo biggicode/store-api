056211011c45786c5a1bece114cd092b648a0f18
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-23 19:54:18 +0200

    show just name and price and limit it to 4

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,16 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("-name price");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(4)
+    .skip(1);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,12 +24,17 @@ const getAllProducts = async (req, res) => {
   }
 
   let result = Product.find(queryObject);
-
+  //sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
 
   const products = await result;
