2e3baeac4eb7deac6fe94030f5dc71f59d8f1750
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-23 23:32:11 +0200

    get product with price greater than

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("name").select("name price");
+  const products = await Product.find({
+    price: {
+      $gt: 30,
+    },
+  })
+    .sort("price")
+    .select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
